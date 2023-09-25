aa424f78ec1775c154e89faba0e0a1ef691c28f3
md2228 <md2228@protonmail.com>
2023-09-25 20:42:28 +0300

    No deviations from the original excluding the consumer

---
+++ UncreativeProjectName1/Data/IssueDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UncreativeProjectName1.Models;
+
+namespace UncreativeProjectName1.Data
+{
+    public class IssueDbContext : DbContext
+    {
+        public IssueDbContext(DbContextOptions<IssueDbContext> options) 
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Issue> Issues { get; set; }
+    }
+}
---
+++ UncreativeProjectName1/Models/Issue.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UncreativeProjectName1.Models
+{
+    public class Issue
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public Priority PriorityField { get; set; }
+        public IssueType IssueTypeField { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Completed { get; set; }
+
+        public enum Priority 
+        { 
+            Low, Medium, High 
+        }
+
+        public enum IssueType
+        {
+            Feature, Bug, Documentation
+        }
+    }
+}
